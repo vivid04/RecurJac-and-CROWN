fix the kerastensor  can't be gradient problem

keras_test.py
@@ -67,7 +67,9 @@ class VariationalAutoEncoder(layers.Layer):
 
 # Our model.
 vae = VariationalAutoEncoder(original_dim=784, intermediate_dim=64, latent_dim=32)
-
+import sys
+print("exit")
+sys.exit(0)
 # Loss and optimizer.
 loss_fn = tf.keras.losses.MeanSquaredError()
 optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)

mnist_cifar_models.py
@@ -124,7 +124,8 @@ class NLayerModel:
         self.num_labels = 10
 
         model = Sequential()
-        model.add(Flatten(input_shape=(image_size, image_size, image_channel)))
+        tm = Flatten(input_shape=(image_size, image_size, image_channel))
+        model.add(tm)
         # list of all hidden units weights
         self.U = []
         n = 0
@@ -164,10 +165,12 @@ class NLayerModel:
                     [model.layers[0].input], [layer.output]))
 
         # a tensor to get gradients
+        #tf.disable_eager_execution()
+        import tensorflow
         self.gradients = []
         for i in range(model.output.shape[1]):
             output_tensor = model.output[:, i]
-            self.gradients.append(K.gradients(output_tensor, model.input)[0])
+            self.gradients.append(tensorflow.keras.backend.gradients(output_tensor, model.input)[0])
 
         self.layer_outputs = layer_outputs
         self.model = model